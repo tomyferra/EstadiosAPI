ee8b24bc32d0287f899a553a59e3a07fbc007662
tomyferra <ferra.tomy@gmail.com>
2024-02-13 10:58:07 -0300

    added PstgreSQL

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator finctions
+"""
+
+
+def add(x, y):
+    """Add x and y and return the sum"""
+    return (x+y)
+
+
+def subtract(x, y):
+    """subtract x and y and return the result"""
+    return (x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+sample tests
+"""
+
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """test the calc modules"""
+
+    def test_add_numbers(self):
+        """Adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Substracting numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
